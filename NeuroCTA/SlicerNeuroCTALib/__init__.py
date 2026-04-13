21aadd1ba661e78efaeeaace00fc9c36075585a5
gso5699 <gloria.jso5699@gmail.com>
2026-04-13 10:04:34 -0400

    Change import order

--- NeuroCTA/SlicerNeuroCTALib/__init__.py
+++ NeuroCTA/SlicerNeuroCTALib/__init__.py
@@ -2,6 +2,6 @@ from .Parameter import Parameter
 from .InstallLogic import InstallLogic
 from .Logic import Logic
 from .VesselTableManager import VesselTableManager
-from .Widget import Widget
 from .Signal import Signal
 from .VesselGNN import VesselGNN
+from .Widget import Widget
